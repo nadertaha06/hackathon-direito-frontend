b7e64250bf3678fc023ede486e4cfcfd0567a497
Nader Taha <tahanader068@gmail.com>
2026-05-19 22:23:01 -0300

    initialize project with React, TypeScript, and Vite setup

---
+++ src/custom.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+declare module "*.svg" {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
